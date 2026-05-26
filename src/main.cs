952bab6ad37a6f6f4edae108b71976e9d8c848cd
Dan <contact@danmyers.net>
2026-05-26 16:20:03 -0500

    webapi template

---
+++ src/main.cs
@@ -0,0 +1,15 @@
+namespace UrlShortener;
+
+public class ShortUrl {
+  public int Id { get; set; }
+  public string Code { get; set; }
+  public string LongUrl { get; set; }
+  public int ClickCount { get; set; }
+  public DateTime CreatedAt { get; set; }
+}
+
+class Program {
+  static void Main(string[] args) {
+    
+  }
+}
