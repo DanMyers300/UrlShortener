2391efb1ca674b510175231c9b1f7bd51411e686
Dan <contact@danmyers.net>
2026-05-27 20:43:00 -0500

    started on building the short url

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using UrlShortener.Data;
 using UrlShortener.Models;
+using System.Security.Cryptography;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlite("Data Source=urlshortener.db"));
@@ -22,6 +23,19 @@ app.MapPost("/shorten", async (ShortenRequest req, AppDbContext db) => {
 
   if (uri.Scheme != Uri.UriSchemeHttp && uri.Scheme != Uri.UriSchemeHttps) {
     return Results.BadRequest("Must be http/https link");
+  }
+
+  bool exists = await db.ShortUrls.FirstOrDefaultAsync(s => s.LongUrl == url);
+
+  if (exists) {
+    // To-Do: Return existing short code
+  }
+
+  var allowedChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+  for (int i = 0; i < 5; i++) {
+    var index = RandomNumberGenerator.GetInt32(0, allowedChars.Length);
+    // To-Do: Finish building short url string
   }
 
   return Results.Ok();
